7d1011db7386e14d2096bdd563aac1213a18c38e
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-04-05 15:46:09 +0700

    # fix useTheme to be client side

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,12 +1,32 @@
-export default function useTheme() {
-    let theme: "light" | "dark" = localStorage.getItem('theme') as "light" | "dark"; // light / dark
+"use client";
 
-    if (!theme) { // if not set yet
-        localStorage.setItem('theme', 'light'); // default will be light
-        theme = 'light';
+import { useEffect, useState } from "react";
+
+export default function useTheme() {
+    const [theme, setTheme] = useState<"light"|"dark">("light"); // light / dark
+
+    useEffect(() => {
+        const currTheme = localStorage.getItem("theme") as "light" | "dark" | null;
+
+        const initialTheme = currTheme || "light";
+        document.documentElement.classList.toggle("dark", initialTheme === "dark");
+
+        if (!currTheme) {
+            localStorage.setItem("theme", initialTheme);
+        }
+        setTheme(initialTheme);
+    }, []);
+
+    const toggleTheme = () => {
+        const newTheme: "dark" | "light" = theme === "dark" ? "light" : "dark";
+
+        setTheme(newTheme);
+        localStorage.setItem("theme", newTheme);
+        document.documentElement.classList.toggle("dark", newTheme === "dark");
     }
 
     return {
         theme,
+        toggleTheme,
     };
 }
