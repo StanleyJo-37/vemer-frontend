9086e32ed76d9e1a9a231eb8da712e36c85fcb83
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-04-05 15:45:31 +0700

    # fix check if AuthContext is called under a provider

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,5 +2,8 @@ import AuthContext from "@/context/AuthContext";
 import { useContext } from "react";
 
 export default function useAuth() {
+    const context = useContext(AuthContext);
+    if (!context) throw new Error("useAuth must be used within an AuthProvider");
+    
     return useContext(AuthContext);
 }
