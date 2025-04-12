4f84a5f1a629a3f46cca919b628aa0a4cc08e5a5
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-04-12 23:07:58 +0700

    # feat react-loader-spinner deps

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,9 @@
 "use server";
 
 export default async function Page() {
+
     return (
         <div className="flex items-center justify-items-center min-h-screen w-full gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] !m-0">
-            
         </div>
     );
 }
