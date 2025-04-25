9c37ea566b1df6dd3a5968640f33db6c7d788fe2
Cavinee <roderich.chow@binus.ac.id>
2025-04-25 13:13:28 +0700

    # fix enhance register form styling

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,14 +1,14 @@
 "use server";
 
-import { Button } from "@/components/ui/button";
 import RegisterForm from "./register-form";
 
 export default async function Page() {
     
     return (
-        <div className="flex flex-col justify-center items-center">
-            <h1 className="text-center">Daftar Sekarang</h1>
-            <RegisterForm />
+        <div className="flex flex-col justify-center items-center !bg-white">
+            <div className="shadow-input mx-auto w-full max-w-md rounded-none bg-white p-4 md:rounded-2xl md:p-8 dark:bg-black">
+                <RegisterForm />
+            </div>
         </div>
     );
 }
