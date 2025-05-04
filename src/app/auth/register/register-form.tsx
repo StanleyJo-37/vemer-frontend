5a0e7dbf244a2212f6e53636eeb365b60a9f9213
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-05-04 16:42:21 +0700

    # fix prettier

--- src/app/auth/register/register-form.tsx
+++ src/app/auth/register/register-form.tsx
@@ -1,35 +1,56 @@
-"use client"
+"use client";
 
-import type React from "react"
+import type React from "react";
 
-import { Form, FormField, FormItem, FormLabel } from "@/components/ui/form"
-import { useForm } from "react-hook-form"
-import z from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Input } from "@/components/ui/input"
-import { useEffect, useState } from "react"
-import { Checkbox } from "@/components/ui/checkbox"
-import AuthAPI from "@/api/AuthAPI"
-import { toast } from "sonner"
-import { useRouter } from "next/navigation"
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
-import type { SocialiteProvider } from "@/types/AuthType"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
+import { Checkbox } from "@/components/ui/checkbox";
+import AuthAPI from "@/api/AuthAPI";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import type { SocialiteProvider } from "@/types/AuthType";
 import { icons } from "lucide-react";
-import { AxiosError } from "axios"
-import { Label } from "@/components/ui/label"
-import { cn } from "@/lib/utils"
-import AnimatedButton from "@/components/animated-button"
-import { omit } from "lodash";
+import { AxiosError } from "axios";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+import AnimatedButton from "@/components/animated-button";
+import { Omit, omit } from "lodash";
 import { UserType } from "@/types/UserType";
+import LucideIcon from "@/components/lucide-icon";
+import { Button } from "@/components/ui/button";
 
 const RegisterFormSchema = z.object({
   email: z.string().email().min(1, { message: "Email wajib diisi." }),
   username: z.string().min(1, { message: "Username wajib diisi." }),
   password: z.string().min(1, { message: "Password wajib diisi." }),
-  "toc-accept": z.boolean().refine((val) => val, { message: "Anda harus menyetujui syarat penggunaan Vemer." }),
-})
+  "toc-accept": z
+    .boolean()
+    .refine((val) => val, {
+      message: "Anda harus menyetujui syarat penggunaan Vemer.",
+    }),
+});
 
-const sso: { label: string; provider: SocialiteProvider; icon: keyof typeof icons }[] = [
+const sso: {
+  label: string;
+  provider: SocialiteProvider;
+  icon: keyof typeof icons;
+}[] = [
   {
     label: "Google",
     provider: "google",
@@ -40,180 +61,193 @@ const sso: { label: string; provider: SocialiteProvider; icon: keyof typeof icon
     provider: "linkedin-openid",
     icon: "Linkedin",
   },
-]
+];
+
+type RegisterPayload = Omit<z.infer<typeof RegisterFormSchema>, "toc-accept">;
 
 export default function RegisterForm() {
-    const [isPasswordShown, setIsPasswordShown] = useState<boolean>(false);
-    const [isSubmitLoading, setIsSubmitLoading] = useState<boolean>(false);
-    const [isTocAcceptDialogOpen, setIsTocAcceptDialogOpen] = useState<boolean>(false);
-    const [ssoProvider, setSsoProvider] = useState<SocialiteProvider>();
+  const [isPasswordShown, setIsPasswordShown] = useState<boolean>(false);
+  const [isSubmitLoading, setIsSubmitLoading] = useState<boolean>(false);
+  const [isTocAcceptDialogOpen, setIsTocAcceptDialogOpen] =
+    useState<boolean>(false);
+  const [ssoProvider, setSsoProvider] = useState<SocialiteProvider>();
 
-    const router = useRouter();
-    
-    const form = useForm<z.infer<typeof RegisterFormSchema>>({
-        resolver: zodResolver(RegisterFormSchema),
-        defaultValues: {
-            'email': "",
-            'username': "",
-            'password': "",
-            'toc-accept': false,
-        },
-    });
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof RegisterFormSchema>>({
+    resolver: zodResolver(RegisterFormSchema),
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+      "toc-accept": false,
+    },
+  });
 
   useEffect(() => {
-    if (!ssoProvider) return
+    if (!ssoProvider) return;
 
     const ssoLogin = async () => {
       try {
-        const resp = await AuthAPI.loginSSO(ssoProvider, "", "")
+        const resp = await AuthAPI.loginSSO(ssoProvider, "", "");
       } catch (err) {
         if (err instanceof AxiosError) {
         }
       } finally {
-        setSsoProvider(undefined)
+        setSsoProvider(undefined);
       }
+    };
+
+    ssoLogin();
+  }, [ssoProvider]);
+
+  const onSubmit = async (data: z.infer<typeof RegisterFormSchema>) => {
+    setIsSubmitLoading(true);
+
+    try {
+      const payload: RegisterPayload = omit(data, "toc_accept");
+
+      const resp = await AuthAPI.register(payload);
+
+      const respData = resp.data as UserType;
+
+      localStorage.removeItem("user");
+      localStorage.setItem("user", JSON.stringify(respData));
+
+      router.push("/auth/profile-completion");
+    } catch (err) {
+      toast("Terjadi kesalahan saat registrasi.");
+    } finally {
+      setIsSubmitLoading(false);
     }
+  };
 
-    ssoLogin()
-  }, [ssoProvider])
+  return (
+    <Form {...form}>
+      <Dialog
+        open={isTocAcceptDialogOpen}
+        onOpenChange={() => setIsTocAcceptDialogOpen(false)}
+      >
+        <div className="bg-background">
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Syarat Penggunaan Vemer</DialogTitle>
+            </DialogHeader>
+          </DialogContent>
+        </div>
+      </Dialog>
 
-    const onSubmit = async(data: z.infer<typeof RegisterFormSchema>) => {
-        setIsSubmitLoading(true);
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-4 flex flex-col justify-center"
+      >
+        <FormField
+          disabled={isSubmitLoading}
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Email"
+                  {...field}
+                  disabled={isSubmitLoading}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
 
-        try {
-            const payload: RegisterPayload = omit(data, 'toc_accept');
-            
-            const resp = await AuthAPI.register(payload);
+        <FormField
+          disabled={isSubmitLoading}
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Username"
+                  {...field}
+                  disabled={isSubmitLoading}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
 
-            const respData = resp.data as UserType;
-
-            localStorage.removeItem('user');
-            localStorage.setItem('user', JSON.stringify(respData));
-
-            router.push('/auth/profile-completion');
-        } catch (err) {
-            toast("Terjadi kesalahan saat registrasi.");
-        } finally {
-            setIsSubmitLoading(false);
-        }
-    }
-    
-    return (
-        <Form {...form}>
-            <Dialog open={isTocAcceptDialogOpen} onOpenChange={() => setIsTocAcceptDialogOpen(false)}>
-                <div className="bg-background">
-                    <DialogContent>
-                        <DialogHeader>
-                            <DialogTitle>Syarat Penggunaan Vemer</DialogTitle>
-                        </DialogHeader>
-                        
-                    </DialogContent>
+        <FormField
+          disabled={isSubmitLoading}
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <FormControl>
+                <div className="relative h-auto">
+                  <Input
+                    type={isPasswordShown ? "text" : "password"}
+                    placeholder="Password"
+                    {...field}
+                    disabled={isSubmitLoading}
+                  />
+                  <LucideIcon
+                    icon={isPasswordShown ? "EyeOff" : "Eye"}
+                    className="absolute right-4 top-[.67rem] cursor-pointer w-4 h-4"
+                    onClick={() => setIsPasswordShown((prev) => !prev)}
+                  />
                 </div>
-            </Dialog>
-            
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 flex flex-col justify-center">
-                <FormField
-                    disabled={isSubmitLoading}
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel htmlFor="email">
-                                Email
-                            </FormLabel>
-                            <FormControl>
-                                <Input placeholder="Email" {...field} disabled={isSubmitLoading} />
-                            </FormControl>
-                        </FormItem>
-                    )}
-                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
 
-                <FormField
-                    disabled={isSubmitLoading}
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel htmlFor="username">
-                                Username
-                            </FormLabel>
-                            <FormControl>
-                                <Input placeholder="Username" {...field} disabled={isSubmitLoading} />
-                            </FormControl>
-                        </FormItem>
-                    )}
-                />
-                
-                <FormField
-                    disabled={isSubmitLoading}
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel htmlFor="password">
-                                Password
-                            </FormLabel>
-                            <FormControl>
-                                <div className="relative h-auto">
-                                    <Input
-                                        type={isPasswordShown ? "text" : "password"}
-                                        placeholder="Password"
-                                        {...field}
-                                        disabled={isSubmitLoading}
-                                    />
-                                    <LucideIcon
-                                        icon={isPasswordShown ? "EyeOff" : "Eye"}
-                                        className="absolute right-4 top-[.67rem] cursor-pointer w-4 h-4"
-                                        onClick={() => setIsPasswordShown(prev => !prev)}
-                                    />
-                                </div>
-                            </FormControl>
-                        </FormItem>
-                    )}
-                />
+        <div className="flex flex-col space-y-2">
+          <h2>Atau daftar dengan:</h2>
+          <div className="flex flex-col md:flex-row items-center justify-center space-x-4">
+            {sso.map((_sso, idx) => (
+              <AnimatedButton
+                key={idx}
+                onClick={() => setSsoProvider(_sso.provider)}
+                type="button"
+                icon={_sso.icon}
+                disabled={ssoProvider === _sso.provider}
+                loading={ssoProvider === _sso.provider}
+              >
+                {_sso.label}
+              </AnimatedButton>
+            ))}
+          </div>
+        </div>
 
-                <div className="flex flex-col space-y-2">
-                    <h2>Atau daftar dengan:</h2>
-                    <div className="flex flex-col md:flex-row items-center justify-center space-x-4">
-                        {
-                            sso.map((_sso, idx) => (
-                                <AnimatedButton
-                                    key={idx}
-                                    onClick={() => setSsoProvider(_sso.provider)}
-                                    type="button"
-                                    icon={_sso.icon}
-                                    disabled={ssoProvider === _sso.provider}
-                                    loading={ssoProvider === _sso.provider}
-                                >
-                                    {_sso.label}
-                                </AnimatedButton>
-                            ))
-                        }
-                    </div>
-                </div>
-
-                <FormField
-                    disabled={isSubmitLoading}
-                    control={form.control}
-                    name="toc_accept"
-                    render={({ field }) => (
-                        <FormItem className="flex flex-row items-center justify-center p-2 space-x-4">
-                            <Checkbox
-                                disabled={isSubmitLoading}
-                                checked={field.value}
-                                onCheckedChange={field.onChange}
-                            />
-                            <FormLabel className="!mt-0" htmlFor="toc_accept">Saya menyetujui <span onClick={() => setIsTocAcceptDialogOpen(true)} className="text-blue-600 hover:text-blue-900">syarat penggunaan Vemer</span>.</FormLabel>
-                        </FormItem>
-                    )}
-                />
-
-                <Button
-                    type="submit"
+        <FormField
+          disabled={isSubmitLoading}
+          control={form.control}
+          name="toc-accept"
+          render={({ field }) => (
+            <FormItem className="flex flex-row items-center justify-center p-2 space-x-4">
+              <Checkbox
+                disabled={isSubmitLoading}
+                checked={field.value}
+                onCheckedChange={field.onChange}
+              />
+              <FormLabel className="!mt-0" htmlFor="toc_accept">
+                Saya menyetujui{" "}
+                <span
+                  onClick={() => setIsTocAcceptDialogOpen(true)}
+                  className="text-blue-600 hover:text-blue-900"
                 >
-                    Daftar
-                </Button>
-            </form>
-        </Form>
-    );
+                  syarat penggunaan Vemer
+                </span>
+                .
+              </FormLabel>
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Daftar</Button>
+      </form>
+    </Form>
+  );
 }
