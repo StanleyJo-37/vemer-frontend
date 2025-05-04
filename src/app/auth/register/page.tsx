18adb1603b4461b1c555352d5ed473502e667ecd
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-05-04 16:37:15 +0700

    # feat Metadata

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,13 +1,36 @@
 "use server";
 
-import { Button } from "@/components/ui/button";
+import { Metadata } from "next";
 import RegisterForm from "./register-form";
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        openGraph: {
+            siteName: "Vemer",
+            title: "Register",
+            description: "Buat akun baru untuk menggunakan layanan Vemer.",
+            type: "website",
+            locale: "id_ID",
+        },
+        title: "Register",
+        description: "Buat akun baru untuk menggunakan layanan Vemer.",
+        keywords: "Vemer, register vemer, buat akun vemer",
+        robots: "index, follow",
+        other: {
+            ["og:site_name"]: "Vemer",
+            ["og:title"]: "Register",
+            ["og:description"]: "Buat akun baru untuk menggunakan layanan Vemer.",
+            ["og:type"]: "website",
+            ["og:locale"]: "id_ID",
+        }
+    };
+}
 
 export default async function Page() {
     
     return (
         <div className="flex flex-col justify-center items-center">
-            <h1 className="text-center">Daftar Sekarang</h1>
+            <h1 className="text-center text-3xl my-8">Daftar Sekarang</h1>
             <RegisterForm />
         </div>
     );
