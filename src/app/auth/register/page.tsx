aaafad9684c19bc5ad7d938a0dd649da97b97176
Cavinee <roderich.chow@binus.ac.id>
2025-05-08 07:33:40 +0700

    # fix added sky-blue styling and box-shadow

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -30,9 +30,7 @@ export default async function Page() {
     
     return (
         <div className="flex flex-col justify-center items-center !bg-white">
-            <div className="shadow-input mx-auto w-full max-w-md rounded-none bg-white p-4 md:rounded-2xl md:p-8 dark:bg-black">
-                <SignupForm />
-            </div>
+            <SignupForm />
         </div>
     );
 }
