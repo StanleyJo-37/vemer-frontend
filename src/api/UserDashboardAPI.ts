51a8832d00a3a5477e8031ea4242e57b9015dcca
123jason689 <jason.melvin689@gmail.com>
2025-06-08 18:08:55 +0700

    # fix merge error

--- src/api/UserDashboardAPI.ts
+++ src/api/UserDashboardAPI.ts
@@ -3,8 +3,71 @@ import API from "./axios";
 import { UserFormat } from "@/components/leaderboard";
 import { AxiosResponse } from "axios";
 
+const baseUrl = "/dashboard/user"
+
 const UserDashboardAPI = {
-  totalActivities: async (category: string): Promise<UserFormat[]> => {
+    attendedActivities: async ():Promise<{data:number}> => {
+        return API.AuthenticatedAPI.request({
+            url: baseUrl+'/attended-activities',
+            method: 'GET',
+        });
+    },
+    totalPoints: async ():Promise<{data:number}> => {
+        return API.AuthenticatedAPI.request({
+            url: baseUrl+'/total-points',
+            method: 'GET',
+        });
+    },
+    getRank: async () => {
+        return API.AuthenticatedAPI.request({
+            url: baseUrl+'/get-rank',
+            method: 'GET',
+        });
+    },
+    upcomingActivities: async () => {
+        return API.AuthenticatedAPI.request({
+            url: baseUrl+'/upcoming-activities',
+            method: 'GET',
+        });
+    },
+    announcements: async () => {
+        return API.AuthenticatedAPI.request({
+            url: baseUrl+'/announcements',
+            method: 'GET',
+        });
+    },
+    recommendedActivities: async () => {
+        return API.AuthenticatedAPI.request({
+            url: baseUrl+'/recommended-activities',
+            method: 'GET',
+        });
+    },
+    recentActivities: async () => {
+        return API.AuthenticatedAPI.request({
+            url: baseUrl+'/recent-activities',
+            method: 'GET',
+        });
+    },
+    badges: async () => {
+        return API.AuthenticatedAPI.request({
+            url: baseUrl+'/badges',
+            method: 'GET',
+        });
+    },
+    favouriteBadges: async () => {
+        return API.AuthenticatedAPI.request({
+            url: baseUrl+'/favourite-badges',
+            method: 'GET',
+        });
+    },
+    setFavouriteBadges: async (data: any) => {
+        return API.AuthenticatedAPI.request({
+            url: baseUrl+'/set-favourite-badges',
+            method: 'POST',
+            data,
+        });
+    },
+      totalActivities: async (category: string): Promise<UserFormat[]> => {
     return API.AuthenticatedAPI.request({
       url: "/leaderboard/user",
       method: "GET",
@@ -70,75 +133,6 @@ const UserDashboardAPI = {
       },
     });
   },
-};
-
-export default UserDashboardAPI;
-import API from "./axios";
-
-const baseUrl = "/dashboard/user"
-
-const UserDashboardAPI = {
-    attendedActivities: async ():Promise<{data:number}> => {
-        return API.AuthenticatedAPI.request({
-            url: baseUrl+'/attended-activities',
-            method: 'GET',
-        });
-    },
-    totalPoints: async ():Promise<{data:number}> => {
-        return API.AuthenticatedAPI.request({
-            url: baseUrl+'/total-points',
-            method: 'GET',
-        });
-    },
-    getRank: async () => {
-        return API.AuthenticatedAPI.request({
-            url: baseUrl+'/get-rank',
-            method: 'GET',
-        });
-    },
-    upcomingActivities: async () => {
-        return API.AuthenticatedAPI.request({
-            url: baseUrl+'/upcoming-activities',
-            method: 'GET',
-        });
-    },
-    announcements: async () => {
-        return API.AuthenticatedAPI.request({
-            url: baseUrl+'/announcements',
-            method: 'GET',
-        });
-    },
-    recommendedActivities: async () => {
-        return API.AuthenticatedAPI.request({
-            url: baseUrl+'/recommended-activities',
-            method: 'GET',
-        });
-    },
-    recentActivities: async () => {
-        return API.AuthenticatedAPI.request({
-            url: baseUrl+'/recent-activities',
-            method: 'GET',
-        });
-    },
-    badges: async () => {
-        return API.AuthenticatedAPI.request({
-            url: baseUrl+'/badges',
-            method: 'GET',
-        });
-    },
-    favouriteBadges: async () => {
-        return API.AuthenticatedAPI.request({
-            url: baseUrl+'/favourite-badges',
-            method: 'GET',
-        });
-    },
-    setFavouriteBadges: async (data: any) => {
-        return API.AuthenticatedAPI.request({
-            url: baseUrl+'/set-favourite-badges',
-            method: 'POST',
-            data,
-        });
-    },
 };
 
 export default UserDashboardAPI;
